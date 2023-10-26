c7b63f5a0ed37481d7c92ef372870850e1cb9c72
ElianaSperat <106990216+ElianaSperat@users.noreply.github.com>
2023-10-26 19:49:15 -0300

    Update app.js

--- primerparcial/app.js
+++ primerparcial/app.js
@@ -1,1 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+import path from "path";
+import canciones from "./routes/canciones-routes.js";
+import albumes from "./routes/albumes-routes.js";
 
+mongoose
+    .connect('mongodb://127.0.0.1/taylorverse', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Conectado con la base de datos!!!")
+    })
+    .catch((error) => {
+        console.log("Error al conectar con la Base de datos", error)
+    })
+
+const app = express()
+const __dirname = path.resolve()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.sendFile('./vistas/index.html', {root: __dirname})
+})
+app.use('/canciones', canciones)
+app.use('/albumes', albumes)
+
+
+const puerto = process.env.PORT || 3002;
+
+app.listen(puerto, function() {
+    console.log("Servidor Corriendo")
+})
